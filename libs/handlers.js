8695eabd1e75e286d8a200cd0d1d00b0dc933819
Carlos Timóteo <carlos-e-timoteo@telecom.pt>
2014-10-16 15:16:38 +0100

    Fixed sync

--- libs/handlers.js
+++ libs/handlers.js
@@ -3,20 +3,21 @@ var color = require( 'ansi-color' ).set;
 
 //Load my libs
 var queues = require( './queues.js' );
+var connections = require( './connections.js' );
 
 function setGlobalConnectionHandlers() {
     //Check for connection errors
-    connection.on('error', function() {
+    connection.on('error', function(error) {
         //Log connection errors
-        errors_log.error(error);
+       errors_log.error(error);
     });
     //Start connection
     connection.on('connect', function() {
-        console.log(color('Connection::connect', 'white'));
+        console.log(color( "\nConnection::connect\n", 'white'));
     });
     //Connection is ready
     connection.on('ready', function() {
-        console.log(color('Connection ready...', 'yellow'));
+        console.log(color("\nConnection ready...\n", 'yellow'));
         //Check if are changes in queue
         if (fileActionsQueue.length > 0) {
             console.log( 'There are ' + fileActionsQueue.length + ' changes to process! (2)' );
@@ -27,7 +28,7 @@ function setGlobalConnectionHandlers() {
         connection.sftp(
             function(err, sftp) {
                 if (err) {
-                    console.log("Error, problem starting SFTP: %s", err);
+                    console.log("\nError, problem starting SFTP: %s\n", err);
                     return;
                 }
                 //Use sftp connection in all requests
@@ -40,15 +41,16 @@ function setGlobalConnectionHandlers() {
     });
     //Handle connection end
     connection.on('end', function() {
-        console.log(color('Connection end!', 'red'));
+        console.log(color("\nConnection end!\n", 'red'));
         //Create a new connection again
         connection.resetConnection();
     });
     //Handle connection close
     connection.on('close', function() {
-        console.log(color('Connection closed!', 'red+underlined'));
-        //Create a new connection again
-        connection.resetConnection();
+        console.log(color("\nConnection closed!\n", 'red+underline'));
+        //Create a new connection again...
+
+        eonnections.resetConnection();
     });
 }
 
